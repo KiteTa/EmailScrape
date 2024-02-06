c54d846a911ac923215434cf2c4284a7e2d2a619
KiteTa <mjtang@brandeis.edu>
2024-02-06 14:00:25 -0500

    popup.js updated

--- popup.js
+++ popup.js
@@ -3,4 +3,16 @@ let scrapeEmails = document.getElementById('scrapeEmails');
 
 scrapeEmails.addEventListener("click", () => {
     alert('hello');
+
+    //Get current active tab
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    // Execute script to [arse emails on page
+    chrome.scrpiting.executeScript({
+        target: {tabId: tab.id},
+        func: scrapeEmailsFromPage,
+    });
 })
+
+//Function to scrape Emails
+function scrapeEmailsFromPage() {}
